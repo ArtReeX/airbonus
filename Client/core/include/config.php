b963c2618e2de6fe676a350fd9deeae633b2f489
Arthur Lazarenko <Arthur.Lazarenko@zoho.com>
2017-12-05 17:06:27 +0300

    [UnStable] Добавлен запрет на кеширование страницы (!для разработчиков).

--- Client/core/include/config.php
+++ Client/core/include/config.php
@@ -9,3 +9,6 @@
 <!-- конфигурация страницы -->
 <meta content="text/html; charset=utf-8" />
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+<!-- запрет на кеширование страницы (!для разработчиков) -->
+<meta http-equiv="Cache-Control" content="no-cache">
